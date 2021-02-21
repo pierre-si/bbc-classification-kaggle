712169ab6b312c98c1e324b669dc6858e9373ecf
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-02-21 10:30:15 +0100

    kaggle kernel setup

---
+++ kaggle-notebook.sh
@@ -0,0 +1,10 @@
+kaggle kernels init -p working/
+#edit kernel.json
+kaggle kernels push -p /path/to/kernel
+
+#pull:
+kaggle kernels pull rtatman/list-of-5-day-challenges -p working/
+
+#retrieve output
+kaggle kernels output mrisdal/exploring-survival-on-the-titanic -p output/ 
+
